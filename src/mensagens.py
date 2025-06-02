c1e84f506a777456192dd67bd5721d961094f3e1
Git_Financeiro_EQS <jesse.silva@eqsengenharia.com.br>
2025-06-02 18:49:08 -0300

    Update mensagens.py

--- src/mensagens.py
+++ src/mensagens.py
@@ -17,16 +17,17 @@ Toda vez que virar o mês, é necessário abrir a plataforma FiscalIO e baixar t
 
         self.info3 = "Como operar"
         self.texto3 = """Modo de operar:
- 
-Abra o Microsiga e vá para a rotina "IntAgillitas" no módulo Compras;
- 
-Aberta a rotina, basta dar o play e deixar acontecer!
-Você também tem a opção de lançar apenas uma RT por vez, basta inserir o número da RT no campo próprio da Mariquinha e apertar o botão play logo abaixo.
- 
+        
+Escolha uma das modalidades da Mariquinha, seja o lançamento de uma unica RT ou, o lançamento sequencial, basta dar o play e deixar acontecer!
+Para lançar apenas uma RT por vez basta inserir o número da RT no campo próprio da Mariquinha e apertar o botão play logo abaixo.
+
+Ao acionar a Mariquinha, em qualquer uma de suas modalidades, o navegador será aberto pelo Selenium. A automação abre o microsiga WebApp diretamente, você só precisa inserir as credenciais e selecionar a rotina IntAgillitas no módulo Compras.
+Resumindo, a automação abre o siga WebApp, mas quem coloca as credenciais e seleciona a rotina desejada é você, então muita atenção nessa hora pra não acabar escolhendo a rotina errada. Após aberta a rotina, pode soltar o mouse e o teclado que o resto é com a Mariquinha.
+
 Para interromper ou finalizar a execução do bot, basta levar o cursor do mouse até o limite do canto superior esquerdo da sua tela e aguardar 10 segundos.
 Se a tela do bot estiver preta, mantenha o cursor no canto extremo do monitor e aguarde mais um pouco.
 
-Além disso, a Mariquinha também tem a função de rejeitar Caixas! Basta clicar na boca dela e digitar a mensagem de rejeição desejada. Mas, cuidado! Ela só rejeita o caixa se você já tiver deixado o mesmo aberto na rotina IntAgillitas. Então lembre-se, para utilizar essa funcionalidade é preciso já estar com o microsiga aberto no caixa que se deseja rejeitar.
+Além disso, a Mariquinha também tem a função de rejeitar Caixas! Basta clicar na boca dela e digitar a mensagem de rejeição desejada. Mas, cuidado! Ela só rejeita o caixa se você já o tiver deixado aberto na rotina IntAgillitas. Então, lembre-se, para utilizar essa funcionalidade é preciso já estar com o microsiga WebApp aberto no caixa que se deseja rejeitar.
 """
 
         self.info4 = "Atenção!"
